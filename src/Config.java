f375f656176966b215e9a2383318309c94a2ef0d
Matt <Matt@10.21.79.78>
2018-07-12 15:13:45 -0600

    added and tested config file parsing

---
+++ src/Config.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+public class Config {
+	int numNodes;
+	int interRequestDelay;
+	int csExecutionTime;
+	int numRequests;
+	String [][] nodeIDList;
+	ArrayList<ArrayList<String>> quorumList;
+	
+	Config(String filepath)
+	{ 
+		Scanner sc=null;
+		try {
+		sc = new Scanner(new File (filepath));
+		}
+		catch(Exception e)
+		{
+			System.out.println("Config file not found.");
+		}
+		String line="";
+		int counter1=0;
+		int counter2=0;
+		boolean readFirstLine=false;
+		//iterates through every line in configuration file
+		while (sc.hasNextLine())
+		{
+			line=sc.nextLine();
+			//if line is empty read next line
+			if(line.length()==0)
+			{
+				continue;
+			}
+			char a = line.charAt(0);
+			//check if first char in line is an integer, if not skip line
+			if(a!='0'&&a!='1'&&a!='2'&&a!='3'&&a!='4'&&a!='5'&&a!='6'&&a!='7'&&a!='8'&&a!='9')
+			{
+				continue;
+			}
+			
+			if(readFirstLine==false)
+			{
+				String[] tokens = line.split("\\s+");
+				numNodes=Integer.parseInt(tokens[0]);
+				interRequestDelay=Integer.parseInt(tokens[1]);
+				csExecutionTime=Integer.parseInt(tokens[2]);
+				numRequests=Integer.parseInt(tokens[3]);
+				readFirstLine=true;
+				nodeIDList=new String [numNodes][3];
+				quorumList=new ArrayList<ArrayList<String>>(numNodes);
+				continue;
+			}
+			if(counter1<numNodes) 
+			{
+				String[] tokens = line.split("\\s+");
+				nodeIDList[counter1][0]=tokens[0];
+				nodeIDList[counter1][1]=tokens[1];
+				nodeIDList[counter1][2]=tokens[2];
+				counter1++;
+				continue;
+			}
+			
+			if(counter2<numNodes)
+			{
+				quorumList.add(new ArrayList<String>());
+				String[] tokens = line.split("\\s+");
+				for(int i=0;i<tokens.length;i++)
+				{
+					if(tokens[i].charAt(0)=='#')  //skip rest of line if comment symbol is found
+					{
+						break;
+					}
+					quorumList.get(counter2).add(tokens[i]);
+				}
+				counter2++;
+			}	
+		}
+	}
+
+	public int getNumNodes()
+	{
+		return numNodes;
+	}
+	public int getInterRequestDelay()
+	{
+		return interRequestDelay;
+	}
+	public int getCSExecutionTime()
+	{
+		return csExecutionTime;
+	}
+	public int getNumRequests()
+	{
+		return numRequests;
+	}
+	public String[][] getNodeIDList()
+	{
+		return nodeIDList;
+	}
+	public ArrayList<ArrayList<String>> getQuorumList()
+	{
+		return quorumList;
+	}
+
+}
--- src/Controller.java
+++ src/Controller.java
@@ -2,8 +2,34 @@
 public class Controller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//TODO: remove args override
+		args =new String[1];
+		args[0]="src\\config.txt";
+		
+		
+		Config conf= new Config(args[0]);
+		System.out.print(conf.getNumNodes()+" ");
+		System.out.print(conf.getInterRequestDelay()+" ");
+		System.out.print(conf.getCSExecutionTime()+" ");
+		System.out.println(conf.getNumRequests()+" ");
+		System.out.println("Node ID list:");
+		for(int i=0;i<conf.getNodeIDList().length;i++)
+		{
+			for(int j=0;j<conf.getNodeIDList()[i].length;j++)
+			{
+				System.out.print(conf.getNodeIDList()[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println("Quorum list:");
+		for(int i=0;i<conf.getQuorumList().size();i++)
+		{
+			for(int j=0;j<conf.getQuorumList().get(i).size();j++)
+			{
+				System.out.print(conf.getQuorumList().get(i).get(j)+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
